2b6c88201cdf36ab90127b94325f709dfe5ec6f2
Kyle Thompson <kyle@kylekthompson.com>
2015-03-07 15:29:05 -0500

    Fix admin page auth.. oops..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def authenticate_admin_user!
+  def authenticate_admin!
 	  redirect_to new_user_session_path unless current_user.try(:is_admin?)
 	end
 end
